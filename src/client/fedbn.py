0e3bc3f1aad968c776ace049a6c8acafd9a7714f
KarhouTam <787992532@qq.com>
2023-01-29 11:18:03 +0800

    fix bug: FedBN fix bug of FedBN unable to recongnize the bn params in mobilenetv2

--- src/client/fedbn.py
+++ src/client/fedbn.py
@@ -1,12 +1,16 @@
+from torch.nn import BatchNorm2d
+
 from .fedavg import FedAvgClient
 
 
 class FedBNClient(FedAvgClient):
     def __init__(self, model, args, logger):
         super().__init__(model, args, logger)
-        self.personal_params_name = [
-            name for name in self.model.state_dict().keys() if "bn" in name
-        ]
+        self.personal_params_name = []
+        for module_name, module in self.model.named_modules():
+            if isinstance(module, BatchNorm2d):
+                for param_name, _ in module.named_parameters():
+                    self.personal_params_name.append(f"{module_name}.{param_name}")
         self.init_personal_params_dict = {
             name: param.clone().detach()
             for name, param in self.model.state_dict(keep_vars=True).items()
