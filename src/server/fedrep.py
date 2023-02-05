8e78a2aea4df98d1c4e8f5f042b975ef1e09462c
KarhouTam <787992532@qq.com>
2023-02-05 17:20:47 +0800

    fix method: FedRep

--- src/server/fedrep.py
+++ src/server/fedrep.py
@@ -1,8 +1,8 @@
 from copy import deepcopy
+
 from fedavg import FedAvgServer
 from config.args import get_fedrep_argparser
 from client.fedrep import FedRepClient
-from client.fedavg import FedAvgClient
 
 
 class FedRepServer(FedAvgServer):
@@ -11,47 +11,6 @@ class FedRepServer(FedAvgServer):
         args.finetune_epoch = max(1, args.finetune_epoch)
         super().__init__("FedRep", args, default_trainer=False)
         self.trainer = FedRepClient(deepcopy(self.model), self.args, self.logger)
-
-    def train(self):
-        fedavg_trainer = FedAvgClient(deepcopy(self.model), self.args, self.logger)
-        flag = True
-        for E in self.train_progress_bar:
-            self.current_epoch = E
-
-            if (E + 1) % self.args.verbose_gap == 0:
-                self.logger.log(" " * 30, f"TRAINING EPOCH: {E + 1}", " " * 30)
-
-            if (E + 1) % self.args.test_gap == 0:
-                self.test()
-
-            self.selected_clients = self.client_sample_stream[E]
-
-            delta_cache = []
-            weight_cache = []
-            for client_id in self.selected_clients:
-
-                client_local_params = self.generate_client_params(client_id)
-                if E < (self.args.global_epoch * 0.5):
-                    trainer = fedavg_trainer
-                else:
-                    if flag:
-                        flag = False
-                        self.trainer.personal_params_dict = (
-                            fedavg_trainer.personal_params_dict
-                        )
-                    trainer = self.trainer
-                delta, weight, self.clients_metrics[client_id][E] = trainer.train(
-                    client_id=client_id,
-                    new_parameters=client_local_params,
-                    evaluate=self.args.eval,
-                    verbose=((E + 1) % self.args.verbose_gap) == 0,
-                )
-
-                delta_cache.append(delta)
-                weight_cache.append(weight)
-
-            self.aggregate(delta_cache, weight_cache)
-            self.log_info()
 
 
 if __name__ == "__main__":
