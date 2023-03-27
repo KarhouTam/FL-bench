0a92d36cbad4ba437e7cc4ae2dd1082f36bca785
KarhouTam <787992532@qq.com>
2023-03-27 20:59:10 +0800

    Add comment in FedLC.

--- src/server/fedlc.py
+++ src/server/fedlc.py
@@ -5,7 +5,11 @@ from fedavg import FedAvgServer
 from src.config.args import get_fedlc_argparser
 from src.client.fedlc import FedLCClient
 
-
+# NOTE: The difference between the loss function in this benchmark and the one in the paper.
+# In the paper, the logit of right class is removed from the sum (the denominator).
+# However, I had tried to use the same one in the paper, but the training collapsed.
+# So the reproduction of FedLC is arguable and you should not fully trust it.
+# If you figure out the loss funciton implementation, please open an issue and let me know.
 class FedLCServer(FedAvgServer):
     def __init__(
         self,
