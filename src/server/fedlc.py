ef8c80d7b9a3eb3bd5fc8c18eb209033bc84d8a5
KarhouTam <787992532@qq.com>
2023-04-28 16:12:58 +0800

    📃 docs(FedLC): More Comments

--- src/server/fedlc.py
+++ src/server/fedlc.py
@@ -10,6 +10,7 @@ from src.client.fedlc import FedLCClient
 # However, I had tried to use the same one in the paper, but the training collapsed.
 # So the reproduction of FedLC is arguable and you should not fully trust it.
 # If you figure out the loss funciton implementation, please open an issue and let me know.
+# More discussions about FedLC: https://github.com/KarhouTam/FL-bench/issues/5
 class FedLCServer(FedAvgServer):
     def __init__(
         self,
