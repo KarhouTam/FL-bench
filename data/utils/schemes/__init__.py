3535a9dbdb3ab5ad72cf01b3269fef9581bbb77d
pc <pcemail>
2023-12-02 11:41:22 +0800

    heterogeneous partition for DomainNet

--- data/utils/schemes/__init__.py
+++ data/utils/schemes/__init__.py
@@ -3,11 +3,9 @@ from .randomly_assign_classes import randomly_assign_classes
 from .iid import iid_partition
 from .shards import allocate_shards
 from .semantic import semantic_partition
-from .dirichlet_4_domainnet import dirichlet_4_domainnet
 
 __all__ = [
     "dirichlet",
-    "dirichlet_4_domainnet",
     "randomly_assign_classes",
     "iid_partition",
     "allocate_shards",
