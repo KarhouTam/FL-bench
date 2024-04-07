🐞 fix(CCVR): Miss a `()` in class-wise covariance computation (#64)
Add detection for generating data of absent classes.

src/server/ccvr.py
@@ -9,6 +9,7 @@ from torch.utils.data import Dataset, DataLoader
 
 from fedavg import FedAvgServer
 from src.utils.tools import trainable_params, NestedNamespace
+from src.utils.constants import NUM_CLASSES
 
 
 def get_ccvr_args(args_list=None) -> Namespace:
@@ -37,40 +38,42 @@ class CCVRServer(FedAvgServer):
         features_means, features_covs, features_count = [], [], []
         for client_id in self.train_clients:
             model_params = self.generate_client_params(client_id)
-            means, covs, sizes = self.get_means_covs_from_client(
+            means, covs, counts = self.get_means_covs_from_client(
                 client_id, model_params
             )
             features_means.append(means)
             features_covs.append(covs)
-            features_count.append(sizes)
+            features_count.append(counts)
 
-        num_classes = len(features_count[0])
+        num_classes = NUM_CLASSES[self.args.common.dataset]
         labels_count = [sum(cnts) for cnts in zip(*features_count)]
-        classes_mean = []
-        for c, (means, sizes) in enumerate(
+        classes_mean = [None for _ in range(num_classes)]
+        classes_cov = [None for _ in range(num_classes)]
+        for c, (means, counts) in enumerate(
             zip(zip(*features_means), zip(*features_count))
         ):
-            weights = torch.tensor(sizes, device=self.device) / labels_count[c]
-            means_ = torch.stack(means, dim=-1)
-            classes_mean.append(torch.sum(means_ * weights, dim=-1))
-        classes_cov = [None for _ in range(num_classes)]
+            if sum(counts) > 0:
+                weights = torch.tensor(counts, device=self.device) / labels_count[c]
+                means_ = torch.stack(means, dim=-1)
+                classes_mean[c] = torch.sum(means_ * weights, dim=-1)
         for c in range(num_classes):
-            for k in self.train_clients:
-                if classes_cov[c] is None:
-                    classes_cov[c] = torch.zeros_like(features_covs[k][c])
+            if classes_mean[c] is not None:
+                for k in self.train_clients:
+                    if classes_cov[c] is None:
+                        classes_cov[c] = torch.zeros_like(features_covs[k][c])
 
-                classes_cov[c] += (features_count[k][c] - 1) / (
-                    labels_count[c] - 1
-                ) * features_covs[k][c] + (
-                    features_count[k][c] / (labels_count[c] - 1)
-                ) * (
-                    features_means[k][c].unsqueeze(1)
-                    @ features_means[k][c].unsqueeze(0)
+                    classes_cov[c] += (
+                        (features_count[k][c] - 1) / (labels_count[c] - 1)
+                    ) * features_covs[k][c] + (
+                        features_count[k][c] / (labels_count[c] - 1)
+                    ) * (
+                        features_means[k][c].unsqueeze(1)
+                        @ features_means[k][c].unsqueeze(0)
+                    )
+
+                classes_cov[c] -= (labels_count[c] / (labels_count[c] - 1)) * (
+                    classes_mean[c].unsqueeze(1) @ classes_mean[c].unsqueeze(0)
                 )
-
-            classes_cov[c] -= (labels_count[c] / labels_count[c] - 1) * (
-                classes_mean[c].unsqueeze(1) @ classes_mean[c].unsqueeze(0)
-            )
 
         return classes_mean, classes_cov
 
@@ -79,18 +82,23 @@ class CCVRServer(FedAvgServer):
     ):
         data, targets = [], []
         for c, (mean, cov) in enumerate(zip(classes_mean, classes_cov)):
-            samples = np.random.multivariate_normal(
-                mean.cpu().numpy(), cov.cpu().numpy(), self.args.ccvr.sample_per_class
-            )
-            data.append(torch.tensor(samples, dtype=torch.float, device=self.device))
-            targets.append(
-                torch.ones(
+            if mean is not None and cov is not None:
+                samples = np.random.multivariate_normal(
+                    mean.cpu().numpy(),
+                    cov.cpu().numpy(),
                     self.args.ccvr.sample_per_class,
-                    dtype=torch.long,
-                    device=self.device,
                 )
-                * c
-            )
+                data.append(
+                    torch.tensor(samples, dtype=torch.float, device=self.device)
+                )
+                targets.append(
+                    torch.ones(
+                        self.args.ccvr.sample_per_class,
+                        dtype=torch.long,
+                        device=self.device,
+                    )
+                    * c
+                )
 
         data = torch.cat(data)
         targets = torch.cat(targets)
@@ -118,7 +126,7 @@ class CCVRServer(FedAvgServer):
         )
         criterion = torch.nn.CrossEntropyLoss()
         optimizer = torch.optim.SGD(
-            self.model.parameters(), lr=self.args.common.optimizer.lr
+            trainable_params(self.model.classifier), lr=self.args.common.optimizer.lr
         )
 
         self.model.load_state_dict(self.global_params_dict, strict=False)