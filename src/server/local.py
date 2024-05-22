e1d1d27715b24e4ab37e8e1ed8b0aafb05876b2c
KarhouTam <karhoutam@qq.com>
2024-05-22 20:04:38 +0800

    🐞 fix(method): Fix Local-only

--- src/server/local.py
+++ src/server/local.py
@@ -12,3 +12,9 @@ class LocalServer(FedAvgServer):
         return_diff=False,
     ):
         super().__init__(args, algo, unique_model, use_fedavg_client_cls, return_diff)
+
+    def train_one_round(self):
+        client_packages = self.trainer.train()
+        for client_id, package in client_packages.items():
+            self.clients_personal_model_params[client_id].update(package["regular_model_params"])
+            self.clients_personal_model_params[client_id].update(package["personal_model_params"])
