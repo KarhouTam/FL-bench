fd964ac99ac0e5a8ee7ea7f3a186efb350b15633
dennis-grinwald <grinwald.dennis@gmail.com>
2024-11-14 18:09:41 +0100

    integrate floco_server

--- src/client/floco.py
+++ src/client/floco.py
@@ -1,7 +1,8 @@
 from copy import deepcopy
 from collections import OrderedDict
 from typing import Any
-
+import torch
+from src.utils.metrics import Metrics
 from src.client.fedavg import FedAvgClient
 
 class FlocoClient(FedAvgClient):
@@ -10,59 +11,63 @@ class FlocoClient(FedAvgClient):
         self.pers_model = deepcopy(self.model).to(self.device)
         self.optimizer.add_param_group({"params": self.pers_model.parameters()})
         self.init_optimizer_state = deepcopy(self.optimizer.state_dict())
-        self.floco_plus = if self.args.floco.pers_epoch > 0 else False
+        self.floco_plus = True if self.args.floco.pers_epoch > 0 else False
 
     def set_parameters(self, package: dict[str, Any]):
         super().set_parameters(package)
-        self.sample_from = package["sample_from"]
-        self.subregion_parameters = package["subregion_parameters"]
+        if package["subregion_parameters"]:
+            self.model.set_subregion(package["sample_from"], package["subregion_parameters"])
         self.global_params = OrderedDict(
             (key, param.to(self.device))
             for key, param in package["regular_model_params"].items()
         ).values()
-        self.pers_model.load_state_dict(package["personalized_model_params"])
+        if self.floco_plus:
+            self.pers_model.load_state_dict(package["personalized_model_params"])
 
     def package(self):
         client_package = super().package()
-        client_package["personalized_model_params"] = OrderedDict(
-            (key, param.detach().cpu().clone())
-            for key, param in self.pers_model.state_dict().items()
-        )
+        if self.floco_plus:
+            client_package["personalized_model_params"] = OrderedDict(
+                (key, param.detach().cpu().clone())
+                for key, param in self.pers_model.state_dict().items()
+            )
         return client_package
 
     def fit(self):
         # Train global solution simplex (subregion)
         self.model.train()
         self.dataset.train()
-        for _ in range(self.local_epoch):
-            for x, y in self.trainloader:
-                x, y = x.to(self.device), y.to(self.device)
-                logit = self.model(x, self.sample_from, self.subregion_parameters)
-                loss = self.criterion(logit, y)
-                self.optimizer.zero_grad()
-                loss.backward()
-                self.optimizer.step()
-            if self.lr_scheduler is not None:
-                self.lr_scheduler.step()
+        training_loop(self.model, self.dataset, self.trainloader, self.local_epoch, 
+                      self.optimizer, self.criterion, self.lr_scheduler, self.device)
         if self.floco_plus:
             # Train personalized solution simplex (subregion)
-            for _ in range(self.args.floco.pers_epoch):
-                for x, y in self.trainloader:
-                    x, y = x.to(self.device), y.to(self.device)
-                    logit = self.pers_model(x, self.sample_from, *self.subregion_parameters)
-                    loss = self.criterion(logit, y)
-                    self.optimizer.zero_grad()
-                    loss.backward()
-                    for pers_param, global_param in zip(
-                        self.pers_model.parameters(), self.global_params
-                    ):
-                        if pers_param.requires_grad:
-                            try:
-                                pers_param.grad.data += self.args.floco.lamda * (
-                                    pers_param.data - global_param.data
-                                    )
-                            except:
-                                pass
-                    self.optimizer.step()
-                if self.lr_scheduler is not None:
-                    self.lr_scheduler.step()
+            training_loop(self.pers_model, self.dataset, self.trainloader, self.args.floco.pers_epoch, self.optimizer, 
+                          self.criterion, self.lr_scheduler, self.device, self.global_params, self.args.floco.lamda)
+                
+
+def training_loop(model, dataset, dataloader, local_epoch, optimizer, 
+                  criterion, lr_scheduler, device, reg_model_params=None, lamda=1):
+    model.train()
+    dataset.train()
+    for _ in range(local_epoch):
+        for x, y in dataloader:
+            x, y = x.to(device), y.to(device)
+            logit = model(x)
+            optimizer.zero_grad()
+            loss = criterion(logit, y)
+            if reg_model_params is not None:
+                for pers_param, global_param in zip(
+                    model.parameters(), reg_model_params
+                ):
+                    if pers_param.requires_grad:
+                        try:
+                            pers_param.grad.data += lamda * (
+                                pers_param.data - global_param.data
+                                )
+                        except:
+                            pass
+            loss.backward()
+            optimizer.step()
+        if lr_scheduler is not None:
+            lr_scheduler.step()
+    
--- src/server/floco.py
+++ src/server/floco.py
@@ -1,12 +1,17 @@
-import torch
-import numpy as np
-from typing import Union, Literal
+from typing import Any, Union, Literal
+from argparse import ArgumentParser
+from copy import deepcopy
 
 import torch
-import torch.nn as nn
-from src.utils.models import MODELS
-from src.utils.models import DecoupledModel
+import numpy as np
+from omegaconf import DictConfig
+from sklearn import decomposition
+
+from src.utils.tools import Namespace
+from src.client.floco import FlocoClient
+from src.server.fedavg import FedAvgServer
 from src.utils.constants import NUM_CLASSES
+from src.utils.models import MODELS, DecoupledModel
 
 class SimplexModel(DecoupledModel):
     def __init__(self, args) -> None:
@@ -20,138 +25,122 @@ class SimplexModel(DecoupledModel):
         self.classifier = SimplexLinear(
             num_endpoints=self.args.floco.num_endpoints, in_features=base_model.classifier.in_features, 
             out_features=NUM_CLASSES[self.args.dataset.name], bias=True, seed=self.args.common.seed)
-    def forward(self, x, center, radius):
-        sampled_alpha = sample_L1_ball(center, radius, 1)
+        self.sample_from = "simplex_center"
+    def set_subregion(self, sample_from: str, subregion_parameters: tuple):
+        self.sample_from = sample_from
+        self.center, self.radius = subregion_parameters
+    def forward(self, x):
+        if self.sample_from == "simplex_center":
+            sampled_alpha = np.ones(self.args.floco.num_endpoints) / np.ones(self.args.floco.num_endpoints).sum()
+        else:
+            sampled_alpha = sample_L1_ball(self.center, self.radius, 1)
         set_net_alpha(self.classifier, sampled_alpha)
         return self.classifier(self.base(x))
 
+class FlocoServer(FedAvgServer):
 
-def map_simplex_to_cartesian(simplex_point):
-    """
-    Map a point from standard 2-simplex with vertices: (0,0,1), (0,1,0), (1,0,0),
-    to cartesian triangle: triangle(ABC), A=(0,0), B=(1,0), C=(0,1)
-    """
-    return simplex_point[1:]
+    @staticmethod
+    def get_hyperparams(args_list=None) -> Namespace:
+        parser = ArgumentParser()
+        parser.add_argument("--num_endpoints", type=int, default=1)  # TODO improve terminology
+        parser.add_argument("--tau", type=int, default=100)  # TODO improve terminology
+        parser.add_argument("--rho", type=float, default=0.1)  # TODO improve terminology
+        parser.add_argument("--finetune_region", type=str, default='simplex_center')
+        parser.add_argument("--evaluate_region", type=str, default='simplex_center')
 
+        # Floco+ (only used if pers_epoch > 0)
+        parser.add_argument("--pers_epoch", type=int, default=0)  # TODO improve terminology
+        parser.add_argument("--lamda", type=float, default=1)
 
-# A class that defines a region in the solution simplex with a center, and coordinate mappings between the coordinates simplex <-> cartesian
-class SimplexRegion:
-    """
-    Simplex Region/Clustering Region class. Is defined through its cluster center in both simplex & cartesian coordinates. Can sample from its region given certain rho parameter.
-    """
+        return parser.parse_args(args_list)
 
     def __init__(
         self,
-        region_id,
-        center,
-        rho
+        args: DictConfig,
+        algorithm_name: str = "Floco",
+        unique_model=False,
+        use_fedavg_client_cls=True,
+        return_diff=False,
     ):
-        self.region_id = region_id
-        self.center_simplex = center
-        self.center_cartesian = map_simplex_to_cartesian(self.center_simplex)
-        # Radius of the ball to sample around from
-        self.rho = rho
+        super().__init__(args, algorithm_name, unique_model, use_fedavg_client_cls, return_diff)
+        self.model = SimplexModel(self.args)
+        self.model.check_and_preprocess(self.args)
+        self.init_trainer(FlocoClient)
+        self.projected_points = None
 
-        # Presample alphas to accelerate training
-        sampled_alphas = sample_L1_ball(self.center_simplex, self.rho, 100000)
-        # Normalization, IMPORTANT!
-        self.sampled_alphas = sampled_alphas / sampled_alphas.sum(axis=1).reshape(-1, 1)
-        # Append center
-        self.sampled_alphas = np.concatenate([self.sampled_alphas, [self.center_simplex]])
-        self.alphas_cartesian = [map_simplex_to_cartesian(alpha) for alpha in self.sampled_alphas]
+        if self.args.floco.pers_epoch > 0:
+            self.clients_personalized_model_params = {i: deepcopy(self.model.state_dict()) for i in self.train_clients}
 
-    def get_client_subregion(self):
-        return self.sampled_alphas, self.alphas_cartesian
+    def train_one_round(self):
+        if self.args.floco.tau == self.current_epoch:
+            selected_clients = self.selected_clients  # save selected clients
+            self.selected_clients = self.train_clients  # collect gradients from all clients
+            client_packages = self.trainer.train()
+            self.projected_points = compute_projected_points(client_packages, self.args.floco.num_endpoints, self.return_diff)
+            self.selected_clients = selected_clients  # restore selected clients
 
-    def get_region_center_simplex(self):
-        return self.center_simplex
+        client_packages = self.trainer.train()
 
-    def get_region_center_cartesian(self):
-        return self.center_cartesian
+        if self.args.floco.pers_epoch > 0:
+            for client_id in self.selected_clients:
+                self.clients_personalized_model_params[client_id] = client_packages[client_id]["personalized_model_params"]
 
-class SolutionSimplex:
-    """
-    Solution simplex that keeps track of all simplex regions.
-    """
+        self.aggregate(client_packages)
 
-    def __init__(self, args):
-        self.args = args
-        self.rho = self.args.floco.rho
-        self.num_endpoints = self.args.floco.num_endpoints
+    def package(self, client_id: int):
+        server_package = super().package(client_id)
+        if self.projected_points is None:
+            server_package["sample_from"] = "simplex_center" if self.testing else "simplex_uniform"
+            server_package["subregion_parameters"] = None
+        else:
+            server_package["sample_from"] = "region_center" if self.testing else "region_uniform"
+            server_package["subregion_parameters"] = (self.projected_points[client_id], self.args.floco.rho)
 
-    def set_solution_simplex_regions(
-        self, projected_points, rho
-    ):  
-
-        self.simplex_regions = _compute_solution_simplex_(
-            projected_points=projected_points,
-            rho=self.rho 
-        )
-        self.rho = rho
-        self.client_to_simplex_region_mapping = {}
-        for i, simplex_region in enumerate(self.simplex_regions):
-            self.client_to_simplex_region_mapping[i] = simplex_region.region_id
-    
-    def get_client_subregion(self, client_id):
-        # Get correct simplex region
-        sampled_region_id = self.client_to_simplex_region_mapping[client_id]
-        client_simplex_region = self.simplex_regions[sampled_region_id]
-        sampled_alpha_simplex, sampled_alpha_cartesian = client_simplex_region.get_client_subregion()
-        return sampled_alpha_simplex, sampled_alpha_cartesian, sampled_region_id
-
-    def sample_uniform(self, client_id):
-        # Get correct simplex region
-        sampled_region_id = client_id
-        alpha = np.random.exponential(scale=1.0, size=(100000, self.num_endpoints))
-        sampled_alpha_simplex = alpha / alpha.sum(1).reshape(-1, 1)
-        simplex_center = np.ones(self.num_endpoints) / np.ones(self.num_endpoints).sum()
-        sampled_alpha_simplex = np.concatenate([sampled_alpha_simplex, [simplex_center]])
-        sampled_alpha_cartesian = [map_simplex_to_cartesian(alpha) for alpha in sampled_alpha_simplex]
-        return sampled_alpha_simplex, sampled_alpha_cartesian, sampled_region_id
-
-    def get_client_center(self, client_id):
-        sampled_region_id = self.client_to_simplex_region_mapping[client_id]
-        alpha_simplex = self.simplex_regions[sampled_region_id].get_region_center_simplex()
-        alpha_cartesian = self.simplex_regions[sampled_region_id].get_region_center_cartesian()
-        return [alpha_simplex], alpha_cartesian, sampled_region_id
-
-    def get_simplex_region_centers_cartesian(self):
-        return [simplex_region.get_region_center_cartesian() for simplex_region in self.simplex_regions]
-
-    def get_simplex_region_centers_simplex(self):
-        return [simplex_region.get_region_center_simplex() for simplex_region in self.simplex_regions]
-
-def _compute_solution_simplex_(projected_points, rho):
-    simplex_regions = []
-    for i, tmp_center in enumerate(projected_points):
-        simplex_region = SimplexRegion(
-            region_id=i,
-            center=tmp_center,
-            rho=rho
-        )
-        simplex_regions.append(simplex_region)
-    return simplex_regions
-
-def sample_L1_ball(center, radius, num_samples):
-    dim = len(center)
-    samples = np.zeros((num_samples, dim))
-    for i in range(num_samples):
-        # Generate a point on the surface of the L1 unit ball
-        u = np.random.uniform(-1, 1, dim)
-        u = np.sign(u) * (np.abs(u) / np.sum(np.abs(u)))
-        # Scale the point to fit within the radius
-        r = np.random.uniform(0, radius)
-        samples[i] = center + r * u
-    return samples
+        if self.args.floco.pers_epoch > 0:
+            server_package["personalized_model_params"] = self.clients_personalized_model_params[client_id]
+        return server_package
 
 
-def set_net_alpha(net, alphas: tuple[float, ...]):
-    for m in net.modules():
-        if isinstance(m, SimplexLayer):
-            m.set_alphas(alphas)
+def compute_projected_points(client_packages, num_endpoints, return_diff):
+    model_grad_type = "model_params_diff" if return_diff else "regular_model_params"
+    client_gradient_dict = {}
+    for client_id, package in client_packages.items():
+        # TODO fix
+        weights = [params.cpu().numpy() for params in package[model_grad_type].values()]
+        client_grads = [weights[-i].flatten() for i in range(1, num_endpoints + 1)]
+        client_grads = np.concatenate(client_grads)
+        client_gradient_dict[client_id] = client_grads
+
+    # Sort results
+    client_statistics = np.array([client_gradient_dict[key] for key in sorted(client_gradient_dict.keys())])
+
+    print('Projecting gradients ... ')
+    client_statistics = decomposition.PCA(n_components=num_endpoints).fit_transform(client_statistics)
+    print('... finished PCA')
+
+    # Offset z optimization
+    statistics_over_z = []
+    energies_over_z = []
+    best_z = None
+    last_log_energy = np.inf
+    z_grid = np.linspace(1e-4, 1, 1000)
+    for i, z in enumerate(z_grid):
+        # 2. Optimized Simplex projection
+        final_client_statistics = projection_simplex(client_statistics, z=z)
+        print(final_client_statistics.shape)
+        final_client_statistics /= final_client_statistics.sum(1).reshape(-1, 1)
+        statistics_over_z.append(final_client_statistics)
+        log_energy = compute_riesz_s_energy(final_client_statistics, d=2)
+        if log_energy not in [-np.inf, np.inf]:
+            energies_over_z.append(log_energy)
+            if log_energy < last_log_energy:
+                best_z = i
+                last_log_energy = log_energy
+    print('... finished z optimization')
+    return np.array(statistics_over_z)[best_z]
 
 
-def projection_simplex(V, z=1, axis=None):
+def projection_simplex(V, z=1):  # TODO simplify
     """
     Projection of x onto the simplex, scaled by z:
         P(x; z) = argmin_{y >= 0, sum(y) = z} ||y - x||^2
@@ -162,23 +151,15 @@ def projection_simplex(V, z=1, axis=None):
         axis=1: project each V[i] by P(V[i]; z[i])
         axis=0: project each V[:, j] by P(V[:, j]; z[j])
     """
-    if axis == 1:
-        n_features = V.shape[1]
-        U = np.sort(V, axis=1)[:, ::-1]
-        z = np.ones(len(V)) * z
-        cssv = np.cumsum(U, axis=1) - z[:, np.newaxis]
-        ind = np.arange(n_features) + 1
-        cond = U - cssv / ind > 0
-        rho = np.count_nonzero(cond, axis=1)
-        theta = cssv[np.arange(len(V)), rho - 1] / rho
-        return np.maximum(V - theta[:, np.newaxis], 0)
-
-    elif axis == 0:
-        return projection_simplex(V.T, z, axis=1).T
-
-    else:
-        V = V.ravel().reshape(1, -1)
-        return projection_simplex(V, z, axis=1).ravel()
+    n_features = V.shape[1]
+    U = np.sort(V, axis=1)[:, ::-1]
+    z = np.ones(len(V)) * z
+    cssv = np.cumsum(U, axis=1) - z[:, np.newaxis]
+    ind = np.arange(n_features) + 1
+    cond = U - cssv / ind > 0
+    rho = np.count_nonzero(cond, axis=1)
+    theta = cssv[np.arange(len(V)), rho - 1] / rho
+    return np.maximum(V - theta[:, np.newaxis], 0)
 
 
 def compute_riesz_s_energy(simplex_points, d=2):
@@ -189,55 +170,34 @@ def compute_riesz_s_energy(simplex_points, d=2):
     np.fill_diagonal(dist, np.inf)
     # epsilon which is the smallest distance possible to avoid an overflow during gradient calculation
     # eps = 10 ** (-320 / (d + 2))
-    eps = 1e-4
-    b = dist < eps
-    dist[b] = eps
+    epsilon = 1e-4
+    dist[dist < epsilon] = epsilon
     # select only upper triangular matrix to have each mutual distance once
-    mutual_dist = dist[np.triu_indices(len(simplex_points), 1)]    
+    mutual_dist = dist[np.triu_indices(len(simplex_points), 1)]
     mutual_dist[np.argwhere(mutual_dist == 0).flatten()] = 1e-4
     # calculate the energy by summing up the squared distances
-    energies = (1 / mutual_dist**d)
-    energies = energies[~np.isnan(energies)]
-    energy = energies.sum()
-    log_energy = - np.log(len(mutual_dist)) + np.log(energy)
-    return energy, log_energy
+    energies = (1 / mutual_dist ** d)
+    energy = energies[~np.isnan(energies)].sum()
+    log_energy = -np.log(len(mutual_dist)) + np.log(energy)
+    return log_energy
 
-def sample_model_point_estimate(model, new_model, sampling_point):
-    """
-    Creates a new model with the same architecture as the input model, 
-    but replaces the last layer's ParameterList with a Linear layer using 
-    the (weighted) average of its weights.
-    
-    Parameters:
-    model (nn.Module): The PyTorch model to be processed.
-    
-    Returns:
-    nn.Module: A new model with a Linear layer in place of the ParameterList.
-    """    
-    # Extract the last layer (assuming it's a fully connected layer with ParameterList)
-    last_layer = copy.deepcopy(model.classifier._weights)
-    last_layer_bias = copy.deepcopy(model.classifier.bias)
 
-    # Check if last layer is nn.ParameterList
-    if not isinstance(last_layer, torch.nn.ParameterList):
-        raise TypeError("The last layer must be a ParameterList containing weight tensors.")
-    
-    # Compute the average of the tensors in the ParameterList
-    final_weight = 0
-    for factor, weight in zip(sampling_point, last_layer):
-        final_weight += factor * weight
-    
-    # Replace the last layer in the new model with a Linear layer
-    # Assuming the input features of the linear layer are the same as the last Parameter tensor
-    in_features, out_features = final_weight.size()
-    new_model.classifier = torch.nn.Linear(in_features, out_features, bias=True)
-    
-    # Set the new Linear layer's weights to the averaged weights
-    with torch.no_grad():
-        new_model.classifier.weight = torch.nn.Parameter(final_weight)
-        new_model.classifier.bias = last_layer_bias
-    
-    return new_model
+def sample_L1_ball(center, radius, num_samples):
+    dim = len(center)
+    samples = np.zeros((num_samples, dim))
+    # Generate a point on the surface of the L1 unit ball
+    u = np.random.uniform(-1, 1, dim)
+    u = np.sign(u) * (np.abs(u) / np.sum(np.abs(u)))
+    # Scale the point to fit within the radius
+    r = np.random.uniform(0, radius)
+    samples = center + r * u
+    return samples
+
+
+def set_net_alpha(net, alphas: tuple[float, ...]):
+    for m in net.modules():
+        if isinstance(m, SimplexLayer):
+            m.set_alphas(alphas)
 
 
 def seed_weights(weights: list, seed: int) -> None:
@@ -246,7 +206,7 @@ def seed_weights(weights: list, seed: int) -> None:
         torch.manual_seed(seed + i)
         torch.nn.init.xavier_normal_(weight)
 
-class StandardConv(nn.Conv2d):
+class StandardConv(torch.nn.Conv2d):
     def __int__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -255,7 +215,7 @@ class StandardConv(nn.Conv2d):
         return self
     
 
-class StandardLinear(nn.Linear):
+class StandardLinear(torch.nn.Linear):
     def __int__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -268,10 +228,10 @@ class SimplexLayer:
     def __init__(self, init_weight: torch.Tensor, num_endpoints: int, seed: int):
         self.num_endpoints = num_endpoints
         self._alphas = tuple([1/num_endpoints for _ in range(num_endpoints)])  # set by the train() method each round
-        self._weights = nn.ParameterList([_initialize_weight(init_weight, seed + i) for i in range(num_endpoints)])
+        self._weights = torch.nn.ParameterList([_initialize_weight(init_weight, seed + i) for i in range(num_endpoints)])
 
     @property
-    def weight(self) -> nn.Parameter:
+    def weight(self) -> torch.nn.Parameter:
         return sum(alpha * weight for alpha, weight in zip(self._alphas, self._weights))
 
     def set_alphas(self, alphas: Union[tuple[float], Literal["center"]]):
@@ -281,20 +241,20 @@ class SimplexLayer:
             raise ValueError(f"alphas must match number of simplex endpoints ({self.num_endpoints})")
 
 
-class SimplexLinear(nn.Linear, SimplexLayer):
+class SimplexLinear(torch.nn.Linear, SimplexLayer):
     def __init__(self, num_endpoints: int, seed: int, *args, **kwargs):
         super().__init__(*args, **kwargs)
         SimplexLayer.__init__(self, init_weight=self.weight, num_endpoints=num_endpoints, seed=seed)
 
 
-class SimplexConv(nn.Conv2d, SimplexLayer):
+class SimplexConv(torch.nn.Conv2d, SimplexLayer):
     def __init__(self, num_endpoints: int, seed: int, *args, **kwargs):
         super().__init__(*args, **kwargs)
         SimplexLayer.__init__(self, init_weight=self.weight, num_endpoints=num_endpoints, seed=seed)
 
 
-def _initialize_weight(init_weight: torch.Tensor, seed: int) -> nn.Parameter:
-    weight = nn.Parameter(torch.zeros_like(init_weight))
+def _initialize_weight(init_weight: torch.Tensor, seed: int) -> torch.nn.Parameter:
+    weight = torch.nn.Parameter(torch.zeros_like(init_weight))
     torch.manual_seed(seed)
     torch.nn.init.xavier_normal_(weight)
     return weight
